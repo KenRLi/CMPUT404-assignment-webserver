440b29750af62c914780dd9f9659143938c62c60
Ken Li <krli@ualberta.ca>
2020-01-24 17:19:17 -0700

    What I have so far

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 #  coding: utf-8 
 import socketserver
 
+from http import HTTPStatus
+import os
+
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -26,13 +29,75 @@ import socketserver
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
-
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+
+        # Parse client headers
+        headers = [header.split(" ") for header in self.data.decode().split("\r\n")]
+
+        print(headers)
+
+        self.method = headers[0][0]
+        self.path = headers[0][1]
+        self.protocol = headers[0][2]
+
+        # Only accept GET methods
+        if (self.method == "GET"):
+            # Builds path requested by client, ignoring redundant separators and up-level references
+            BASEPATH = os.getcwd() + "/www"
+            requestedPath = os.path.normpath(BASEPATH + self.path)
+
+            print(requestedPath)
+
+            # Return index.html for paths that end in "/"
+            if (self.path[-1] == "/"):
+                requestedPath += "/index.html"
+
+            # Check if requested path is a file
+            if (os.path.isfile(requestedPath)):
+                # File Found
+                statusCode = HTTPStatus.OK
+                content = open(requestedPath, "r").read()
+                self.sendRequest(statusCode, content)
+
+            else:
+                # No File Found
+                statusCode = HTTPStatus.NOT_FOUND
+                content = open("./templates/404.html", "r").read()
+                self.sendRequest(statusCode, content)
+        else:
+            statusCode = HTTPStatus.METHOD_NOT_ALLOWED
+            content = open("./templates/405.html", "r").read()
+            self.sendRequest(statusCode, "")
+
+    def getMimeType(self):
+        _, fileExt = os.path.splitext(self.path)
+
+        if (fileExt.upper() == ".HTML"):
+            return "Content-Type: text/html\r\n\r\n"
+        elif (fileExt.upper() == ".CSS"):
+            return "Content-Type: text/css\r\n\r\n"
+        else:
+            return "Content-Type: text/html\r\n\r\n"
+
+    def sendRequest(self, statusCode, content):
+        tempContent = open("./templates/501.html", "r").read()
+        response = self.protocol + "501 Not Implemented\r\n" + self.getMimeType() + tempContent
+        
+        if (statusCode == HTTPStatus.OK):
+            print("200")
+            response = self.protocol + "200 OK\r\n" + self.getMimeType() + content
+        elif (statusCode == HTTPStatus.NOT_FOUND):
+            print("404")
+            response = self.protocol + "404 Not Found\r\n" + self.getMimeType() + content
+        elif (statusCode == HTTPStatus.METHOD_NOT_ALLOWED):
+            print("405")
+            response = self.protocol + "405 Method Not Allowed\r\n" + self.getMimeType() + content
+
+        self.request.sendall(bytearray(response, 'utf-8'))
+        
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
